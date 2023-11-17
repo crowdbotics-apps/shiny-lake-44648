c51ed498c704117d7d50aaccfdbb120203450133
Crowdbotics <team@crowdbotics.com>
2023-11-17 20:01:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-lake-44648",
+  url: "https://shiny-lake-44648.botics.co",
+  api: "https://shiny-lake-44648.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
